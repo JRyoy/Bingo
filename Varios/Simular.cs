3bbcc435e29d6a646109d60abf71dc10872672ab
josbert <josbert0708@gmail.com>
2024-04-18 22:28:24 -0300

    ok
    ok

--- Varios/Simular.cs
+++ Varios/Simular.cs
@@ -4,14 +4,23 @@ public class Simular
 {
     public long simularSinHilos(Bolillero bolillero, int CantidadSimulacion, List<int> bolillas)
         => bolillero.JugadaNV(CantidadSimulacion, bolillas);
-    public long simularConHilos(Bolillero bolillero, int CantidadSimulacion,List<int> bolillas,int hilos)
+    public long simularConHilos(Bolillero bolillero, int CantidadSimulacion,List<int> bolillas,long hilos)
     {
-        Task<int>[] tareas = new Task<int>[hilos];
+        Task<long>[] tareas = new Task<long>[hilos];
+        long resultado=0;
         for(long i=0 ;i<hilos; i++)
         {
             Bolillero Clon=bolillero.Clon(bolillero);
-            
+            if(hilos%2==0)
+            {
+                tareas[i] = Task.Run(() => Clon.JugadaNV(CantidadSimulacion,bolillas));
+            }
+            else
+            {
+                tareas[i] = Task.Run(()=> Clon.JugadaNV());
+            }
         }
+            return resultado;
     }
 }
 
